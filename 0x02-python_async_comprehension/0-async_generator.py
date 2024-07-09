17e852112cc06b91fbf0aec883f938ec1a3152e9
Sandrine <99196826+2100032578cse@users.noreply.github.com>
2024-07-10 00:58:04 +0530

    Generates a sequence of 10 random numbers.

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -1,1 +1,18 @@
 #!/usr/bin/env python3
+"""
+Task 0.
+"""
+import asyncio
+import random
+from typing import Generator
+
+
+async def async_generator() -> Generator[float, None, None]:
+    """
+    Generates a sequence of 10 numbers.
+    """
+    count = 0
+    while count < 10:
+        await asyncio.sleep(1)
+        yield random.random() * 10
+        count += 1
