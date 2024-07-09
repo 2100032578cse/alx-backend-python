178dda8102994c9165be567f80e16e9632d6634a
Sandrine <99196826+2100032578cse@users.noreply.github.com>
2024-07-10 01:03:38 +0530

    Creates a list of 10 numbers from the 10-number generator.

--- 0x02-python_async_comprehension/1-async_comprehension.py
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -1,1 +1,20 @@
 #!/usr/bin/env python3
+"""
+task 1
+"""
+from typing import List
+from importlib import import_module as using
+
+
+async_generator = using('0-async_generator').async_generator
+
+
+async def async_comprehension() -> List[float]:
+    """
+    Creates a list of 10 numbers from the 10-number generator.
+    """
+    result = []
+    async for num in async_generator():
+        result.append(num)
+    return result
+
